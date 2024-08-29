657ff7e04d1e471ac23978cc19ad3f31f223f8b1
Prabin Bhattarai <122869241+probeen007@users.noreply.github.com>
2024-08-29 16:31:48 +0545

    Update HomeBanner.tsx
    Some content change

--- app/components/HomeBanner.tsx
+++ app/components/HomeBanner.tsx
@@ -4,9 +4,9 @@ const HomeBanner = () => {
         <div className="relative bg-gradient-to-r from-sky-500 to-sky-700 mb-8">
             <div className="mx-auto px-8 py-12 flex flex-col gap-2 md:flex-row items-center justify-evenly">
                 <div className="mb-8 md:mb-0 text-center">
-                    <h1 className="text-4xl md:text-6xl font-bold text-white mb-4">Summer Sale</h1>
-                    <p className="text-lg md:text-xl text-white mb-2">Enjoy Discounts on selected items</p>
-                    <p className="text-2xl md:text-5xl text-yellow-400 font-bold">Get 50% Off</p>
+                    <h1 className="text-4xl md:text-6xl font-bold text-white mb-4">Bhattarai Ecommerce</h1>
+                    <p className="text-lg md:text-xl text-white mb-2">Explore a Wide Range of Premium Kitchenware</p>
+                    <p className="text-2xl md:text-5xl text-yellow-400 font-bold">Shop Online for Exclusive Offers & Quality Service</p>
                 </div>
                 <div className="w-1/3 relative aspect-video">
                     <Image src='/banner-image.png' fill alt='Banner' className='object-contain'/>
